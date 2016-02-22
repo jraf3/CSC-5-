bc5d3db3589efc66fbfce2b54614bf3f87f5b504
jraf3 <jrafael3@student.rcc.edu>
2016-02-22 10:11:06 -0800

    Added files via upload

---
+++ Lab/hello_world_V2/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: John Mar Rafael
+ *
+ * Created on February 17, 2016, 9:45 AM
+ * Purpose: Our First Program
+ */
+
+//system libraries
+#include <iostream>
+//using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Input Values
+    
+    //Map the inputs to the outputs
+    
+    //Output the results
+    std::cout <<"Hello World"<<std::endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
+
