b09d2b03ef22fea8b8f988207af93a55c0003d9a
jraf3 <jrafael3@student.rcc.edu>
2016-03-28 09:53:00 -0700

    Signed-off-by: jraf3 <jrafael3@student.rcc.edu>

---
+++ Class/Loops/RuleOf72_for/main.cpp
@@ -0,0 +1,49 @@
+/* 
+ * File:   main.cpp
+ * Author: John Mar Rafael
+ * Purpose: To Illustrate rule of 72 by way of a for loop
+ * Created on March 28, 2016, 8:12 AM
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //Declare variables
+    float pv=100.0f; //Present value
+    float iRate=0.05f; //Interest rate
+    float nComp; //Number of compounding periods by rules of 72
+    int nYears=0; //Comparison of calclation to the rule of 72
+    float fv=pv; //Future value
+    
+    //Calculate the approximate number years to double by rule of 72
+    nComp=0.72f/iRate;
+    
+    //Output the initial conditions and setup the table
+    cout<<"Present value = $"<<pv<<endl;
+    cout<<"Interest rate = "<<iRate*100<<"%"<<endl;
+    cout<<"Years Future Value"<<endl;
+    cout<<fixed<<setprecision(2)<<showpoint;
+    cout<<setw(3)<<nYears<<setw(7)<<" $"<<fv<<endl;
+    
+    //Loop each year until the future value is 2x the present value
+    for (nYears=1;fv<2*pv;nYears++){
+        fv*=(1+iRate);//Yearly interest rate calculation
+        cout<<setw(3)<<nYears<<setw(7)<<" $"<<fv<<endl;
+    }
+
+    //Output the results
+    cout<<"By the Rule of 72 it will take "<<nComp<<" years to double"<<endl;
+    cout<<"The Future Value = $"<<fv<<endl;
+    cout<<"The number of years to more than double = "<<nYears<<endl;
+    
+    //Exit stage right
+    
+    return 0;
+}
+
