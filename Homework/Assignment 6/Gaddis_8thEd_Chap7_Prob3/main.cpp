d2602f408565e40e8a766af3028b84bae668528e
jraf3 <jrafael3@student.rcc.edu>
2016-05-16 10:38:47 -0700

    Signed-off-by: jraf3 <jrafael3@student.rcc.edu>

--- Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob3/main.cpp
+++ Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob3/main.cpp
@@ -9,7 +9,6 @@
 
 using namespace std;
 
-
 /*
  * 
  */
@@ -18,16 +17,51 @@ int main(int argc, char** argv) {
     const int SIZE = 5;
     string salsa[SIZE] = {"Mild", "Medium", "Sweet", "Hot", "Zesty"};
     int jars[SIZE];
-    
+
     //Input jars sold for each type
-    for (int index=0; index <SIZE; index++){
-        cout <<"Jars of "<< salsa[index]<<" sold: ";
-        cin>>jars[index];
+    for (int index = 0; index < SIZE; index++) {
+        cout << "Jars of " << salsa[index] << " sold: ";
+        cin >> jars[index];
     }
-    
-    //Calculations
-    
+
+    //Calculations for highest and lowest
+    unsigned int highest;
+    unsigned int lowest;
+
+    int posHigh;
+    int posLow;
+
+    for (int index = 0; index < SIZE; index++) {
+        if (jars[index] > highest) {
+            highest = jars[index];
+            posHigh = index;
+        }
+    }
+
+    for (int index = 0; index < SIZE; index++) {
+        if (jars[index] < lowest) {
+            lowest = jars[index];
+            posLow = index;
+        }
+    }
+    cout << endl << endl;
+
     //Output results
-    cout<<"Jars of salsa sold: ";
+    for (int index = 0; index < SIZE; index++) {
+        cout << "Sales for " << salsa[index] << " salsa: " << jars[index] << endl;
+    }
+
+    cout << endl;
+
+    //sum of jars
+    int sum = 0;
+    for (int index = 0; index < SIZE; index++) {
+        sum += jars[index];
+    }
+
+    cout << "Total amount of jars sold: " << sum << endl;
+    cout << salsa[posHigh] << " salsa sold the most amount of jars!" << endl;
+    cout << salsa[posLow] << " salsa sold the least amount of jars..." << endl;
+
     return 0;
 }
