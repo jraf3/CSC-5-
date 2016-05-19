1539cc1c1d8da5ec29a136b353b43dec0f389565
jraf3 <jrafael3@student.rcc.edu>
2016-05-18 19:01:51 -0700

    Signed-off-by: jraf3 <jrafael3@student.rcc.edu>

--- Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob5/main.cpp
+++ Homework/Assignment 6/Gaddis_8thEd_Chap7_Prob5/main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -17,9 +18,15 @@ int main(int argc, char** argv) {
     const int monkey = 3;//amount of monkeys
     const int day = 5;//weekdays
     float food [monkey][day];//3x5 array
-    float ate = 0;//holds total lbs of food ate
+    float ate = 0;
+    float Tate = 0;//holds total lbs of food ate
     float avg = 0;//holds the average of food ate
     int m, d;//loop counters
+    
+    int least;
+    int most;
+    least = food[0];
+    most = food[0];
     
     //intro
     cout<<"This program will calculate the average, least, and greatest "
@@ -31,13 +38,34 @@ int main(int argc, char** argv) {
         for (d=0;d<day; day++){
             cout <<"Monkey "<< (m+1);
             cout <<", Day "<< (d+1);
-            cin >> ate[m][d];
+            cin >> food[m][d];
             cout <<" Pounds.";
         }
         cout <<endl;
     }
 
+    //Loops to find total and average
+    for (m=0; m<monkey; m++){
+        Tate += ate[m][d];
+        avg = Tate / ate[m][d];
+    }
     
+    //loop for most lbs ate
+    for (m = 1; m <monkey; m++){
+        if (food[m]>most)
+            most = food[m];
+    }
+    
+    //loop for least lbs ate
+    for (m = 1; m <monkey; m++){
+        if (food[m]<least)
+            least = food[m];
+    }
+    
+    cout <<fixed << showpoint<<setprecision(2);
+    cout << "The average amount of lbs ate: "<< avg;
+    cout << "Most amount of lbs ate: "<< most;
+    cout <<"Least amount of lbs ate: "<< least;
     
     return 0;
 }
